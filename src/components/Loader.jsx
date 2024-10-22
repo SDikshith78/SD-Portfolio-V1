73d5553992e731c99afdc7fc615655f58afab065
sdikshith <sdikshith7885@.com>
2024-10-22 19:54:35 +0530

    done

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -19,7 +19,7 @@ const Loader = ({ onFinish }) => {
       setShowVideo(true);
       const videoTimer = setTimeout(() => {
         onFinish(); // Call onFinish to signal Loader completion after video plays
-      }, 3870); // Video duration, adjust as needed
+      }, 3830); // Video duration, adjust as needed
       return () => clearTimeout(videoTimer);
     }
   }, [currentGreeting, onFinish]);
