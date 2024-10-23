6ef5b5d7543bc2adf21d9c739c634b61828b91ae
sdikshith <sdikshith7885@.com>
2024-10-23 11:57:55 +0530

    sai-dikshith portfolio v1

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,12 @@ import { useEffect } from 'react'
 
 const App = () => {
   useEffect(()=>{
-    // Initialize Lenis
 const lenis = new Lenis();
 
-// Listen for the scroll event and log the event data
 lenis.on('scroll', (e) => {
   console.log(e);
 });
 
-// Use requestAnimationFrame to continuously update the scroll
 function raf(time) {
   lenis.raf(time);
   requestAnimationFrame(raf);
